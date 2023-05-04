a52d4473b3e88807b660d3bec7e6e99beb65b6a6
omarlawah <omarxxxxx135@gmail.com>
2023-05-04 16:14:41 +0000

    ..

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-generator
+
+This code snippet is useful to generate a random password of 10 characters and can be used in login forms
--- main.js
+++ main.js
@@ -1,23 +1,24 @@
+let span = document.getElementById('span');
+
 let arr=[];
 const characters = "qwertyuiopasdfghjklzxcvbnm,./;'=-0987654321!@#$%^&*()_+}{";
 let pass = "";
 
 //characters.charAt(Math.floor(Math.random()*characters.length)-1);
 
-for (let i=0;i<11;i++){
+for (let i=0;i<21;i++){
     arr.length=i;
     arr.pop(-1);
-    arr.push(characters.charAt(Math.floor(Math.random()*characters.length)-1));
+    arr.push(characters.charAt(Math.floor(Math.random()*characters.length)));
 }
 pass=arr.join("");
 
 
 
-
-let button= document.getElementById('button');
-let span = document.getElementById('span');
-button.addEventListener('click', function generatePassword(){
+function generatePassword(){
     span.textContent=`${pass}`;
-})
-  
+    return;
+}  
+ 
+
 
