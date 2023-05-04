5f701f71fb09967e275aa03df4f0342bce2732c8
omarlawah <omarxxxxx135@gmail.com>
2023-05-04 17:08:57 +0000

    the complete code for a button that generates a 20 tokens passord

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let span = document.getElementById('span');
-
+let button = document.getElementById('button');
+let refresh = document.getElementById('refresh-btn')
 let arr=[];
 const characters = "qwertyuiopasdfghjklzxcvbnm,./;'=-0987654321!@#$%^&*()_+}{";
 let pass = "";
@@ -11,14 +12,32 @@ for (let i=0;i<21;i++){
     arr.pop(-1);
     arr.push(characters.charAt(Math.floor(Math.random()*characters.length)));
 }
+
 pass=arr.join("");
-
-
 
 function generatePassword(){
     span.textContent=`${pass}`;
+    button.classList.add('display');
+    refresh.classList.remove('display');
     return;
 }  
- 
+
+ function refreshPage(){
+    if(confirm("are you sure you want to regenerate another password?")==true){
+        document.location.reload();
+    }
+ }
 
 
+
+
+//copyrights scripts.
+
+let copy= document.querySelector('#copyrights')
+
+function copyrights(){
+    new Date().getFullYear();
+    copy.innerHTML= `${new Date().getFullYear()}`;
+}
+
+copyrights();
